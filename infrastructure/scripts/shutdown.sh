5164cb7509522c381f48f96c3c47463910bad04c
Kaise White <kaisewhite@gmail.com>
2026-03-20 15:42:21 -0400

    feat: add logging for service update during shutdown process

--- infrastructure/scripts/shutdown.sh
+++ infrastructure/scripts/shutdown.sh
@@ -10,7 +10,7 @@ service_arns=$(aws ecs list-services --cluster "$cluster_name" --output text --q
 # Iterate through each service and set desired count to 0
 for service_arn in $service_arns; do
     service=$(basename "$service_arn")
-
+    printf "Updating service %s to desired count 0...\n" "$service"
     aws ecs update-service --cluster "$cluster_name" --service "$service" --desired-count 0 --force-new-deployment >/dev/null
 done
 
